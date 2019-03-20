5ec31323f8137f2792dc9fa20ccced4054d50096
Vale <vale@MacBook-Pro-001.homenet.telecomitalia.it>
2019-03-20 22:08:28 +0100

    twitter and instagram scraper with node.js

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a simple Node.js Scraper 
+### It pulls data from Instagram and Twitter
+
+- [x] Express Server
+- [x] Saves data to lowdb
+- [x] Uses a cron job to keep track of changes overtime
---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { getInstagramCount, getTwitterCount} from './lib/scraper';
+import db from './lib/db';
+import './lib/cron';
+const app = express();
+
+app.get('/scrape', async(req, res, next) => {
+  console.log('Scraping!!');
+  const [iCount, tCount] = await Promise.all([
+    getInstagramCount(),
+    getTwitterCount()
+  ]);
+  res.json({iCount, tCount});
+});
+
+app.listen(2019, () =>{
+    console.log(`Example App running on port 2019`);
+});
+
