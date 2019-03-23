3ac5be9db79c572622281679fa7f7b63e2cf5cd4
unknown <valent95@gmail.com>
2019-03-23 14:25:36 +0100

    lc

--- frontend/components/Data.js
+++ frontend/components/Data.js
@@ -3,16 +3,14 @@ import { ScrapeContext } from './ScrapeContext';
 import Table from './Table';
 
 export default function Data() {
-   
   const { scrapes } = useContext(ScrapeContext);
-
-    return (
-      <div>
-        <h2>Twitter: </h2>
-        <Table scrapes = {scrapes.twitter}/>
-        <h2>Instagram: </h2>
-        <Table scrapes = {scrapes.instagram}/>
-        <ul />
-      </div>
-    );
+  return (
+    <div>
+      <h2>Twitter:</h2>
+       <Table scrapes={scrapes.twitter} />
+      <h2>Instagram:</h2>
+        <Table scrapes={scrapes.instagram} />
+      <ul />
+    </div>
+  );
 }
